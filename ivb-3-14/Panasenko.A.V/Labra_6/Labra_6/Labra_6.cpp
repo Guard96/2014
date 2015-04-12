c44c1c9ffa7a3883f7ce1dcf60a57060a9f36d25
Pechenqko <creator_it@mail.ru>
2015-04-12 17:58:09 +0300

    improved laba_6

--- ivb-3-14/Panasenko.A.V/Labra_6/Labra_6/Labra_6.cpp
+++ ivb-3-14/Panasenko.A.V/Labra_6/Labra_6/Labra_6.cpp
@@ -1,11 +1,139 @@
-// Labra_6.cpp: ���������� ����� ����� ��� ����������� ����������.
-//
+﻿
+#include <string>
+#include <iostream>
+#include <fstream>
 
-#include "stdafx.h"
+using std::istream;
+using std::ostream;
+using std::cout;
+using std::cin;
+using std::endl;
 
-
-int _tmain(int argc, _TCHAR* argv[])
+class Item
 {
+	friend istream & operator >> (istream &f, Item &i);
+	friend ostream & operator << (ostream &f, const Item &i);
+public:
+	int i, c, m;
+
+	bool operator < (const Item s) {
+		return c < s.c;
+	}
+
+	bool operator >(const Item s) {
+		return c > s.c;
+	}
+};
+
+Item * Base = nullptr;
+int len = 0;
+
+void LoadArray();
+void SaveArray();
+void ShortArray();
+void PrintArray();
+
+Item* find(int index);
+
+
+istream &
+operator >> (istream &f, Item &i) {
+	f >> i.i;
+	f >> i.c;
+	f >> i.m;
+	return f;
+}
+
+ostream &
+operator << (ostream &f, const Item &i) {
+	f << i.i << '\t';
+	f << i.c << '\t';
+	f << i.m << endl;
+	return f;
+}
+
+void
+Load_Array() {
+
+	if (Base != nullptr) {
+		delete[] Base;
+		Base = nullptr;
+	}
+	std::ifstream f("base.txt");
+	f >> len;
+
+	Base = new Item[len];
+	for (int i = 0; i < len; i++)
+		f >> Base[i];
+	f.close();
+
+	int size = sizeof(Item);
+	printf("Выделенно %d байт на элемент!\n", size * len, size);
+}
+
+void
+Save_Array() {
+
+	std::ofstream f("base.txt");
+	f << len << '\n';
+	for (int i = 0; i < len; i++)
+		f << Base[i];
+
+	f.close();
+}
+
+void
+Sort_Array() {
+	for (int j = 1; j <= (len - 1); j++) {
+		for (int i = 0; i < len - j; i++) {
+			if (Base[i] < Base[i + 1]) {
+				Item m = Base[i + 1];
+				Base[i + 1] = Base[i];
+				Base[i] = m;
+			}
+		}
+	}
+}
+
+void
+Print_Array() {
+	cout << "номер\tстоимость\tколличество\n";
+	for (int i = 0; i < len; i++)
+		cout << Base[i];
+}
+
+Item*
+Find(int index) {
+	for (int i = 0; i < len; i++)
+	if (Base[i].i == index)
+		return &Base[i];
+
+	return nullptr;
+}
+
+int
+main()
+{
+	setlocale(0, "Russian");
+	Load_Array();
+	cout <<("Сортировка по стоимости\n");
+	Sort_Array();
+	Print_Array();
+	printf("Введите номер товара:");
+	int n = 0;
+	cin >> n;
+
+	Item * find = Find(n);
+
+	if (find != nullptr)
+		cout << *find;
+	else
+		cout << "товар не найден \n";
+	Save_Array();
+
+	system("pause");
 	return 0;
 }
+
+
 
