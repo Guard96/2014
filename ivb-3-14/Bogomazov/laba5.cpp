501b20cb2bae9dcca12eeaf3f0716d3f93f5e458
prosha <Stepan-bogomazov@mail.ru>
2015-04-13 20:48:20 +0300

    Update laba5.cpp

--- ivb-3-14/Bogomazov/laba5.cpp
+++ ivb-3-14/Bogomazov/laba5.cpp
@@ -1,3 +1,9 @@
-#include <iostream>
-#include <string>
-#include <vector>
+#pragma warning(disable : 4996)
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+
+/* Лаб. работа 2
+ Вариант 6
+ ИВБ-3-14
+ Богомазов Степан */
