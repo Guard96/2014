061ba69f7d57264fd931bc31d3ed41c05a0ed4e8
Андрей <viruszold@gmail.com>
2015-04-14 11:31:30 +0400

    Passed

--- ivb-3-14/Bogomazov/laba4.cpp
+++ ivb-3-14/Bogomazov/laba4.cpp
@@ -15,8 +15,7 @@ void array_in(int array[], int size)
 int sum_arr(int array[], int size)
 {
     int sum = 0;
-    for (int i = 0; i < size; ++i)
-    {
+    for (int i = 0; i < size; ++i) {
         if (array[i] > 0)
             sum += array[i];
     }
@@ -24,15 +23,13 @@ int sum_arr(int array[], int size)
 }
 int product_arr(int array[], int i_min, int i_max)
 {
-    if (i_min > i_max)
-    {
+    if (i_min > i_max) {
         int temp = i_min;
         i_min = i_max;
         i_max = temp;
     }
     int product = 1;
-    for (int i = i_min + 1; i < i_max; ++i)
-    {
+    for (int i = i_min + 1; i < i_max; ++i) {
         product *= array[i];
     }
     return product;
@@ -41,10 +38,8 @@ int max_element(int array[], int size)
 {
     int i_max = 0;
     int max = array[0];
-    for (int i = 1; i < size; ++i)
-    {
-        if (array[i] > max)
-        {
+    for (int i = 1; i < size; ++i) {
+        if (array[i] > max) {
             max = array[i];
             i_max = i;
         }
@@ -55,10 +50,8 @@ int min_element(int array[], int size)
 {
     int i_min = 0;
     int min = array[0];
-    for (int i = 1; i < size; ++i)
-    {
-        if (array[i] < min)
-        {
+    for (int i = 1; i < size; ++i) {
+        if (array[i] < min) {
             min = array[i];
             i_min = i;
         }
