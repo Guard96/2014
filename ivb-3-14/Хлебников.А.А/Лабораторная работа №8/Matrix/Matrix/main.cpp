3322a069ff745c18cbb876d657a265480f63274c
mrkriv <virus_450@mail.ru>
2015-05-24 15:14:43 +0400

    Update main.cpp

--- ivb-3-14/Хлебников.А.А/Лабораторная работа №8/Matrix/Matrix/main.cpp
+++ ivb-3-14/Хлебников.А.А/Лабораторная работа №8/Matrix/Matrix/main.cpp
@@ -1,19 +1,18 @@
 #include <iostream>
 #include <string>
-#include "matrix.h"
+#include "lab8.h"
 #include <ctime>
 #include <cstring>
+
 void
 __test_isSymmetric()
 {
-    Matrix<int> m(100, 100, 0);
-    
-    /** Симметричная. Вариант 12*/
-    /**	Кривошея Михаил **/
-    std::cout << "Матрица " << m << std::endl;
-    std::cout << "Симметрична: " << (m.isSymmetric() ? "Нет" : "Да") << std::endl;
+	Matrix<int> m(4, 4, 0);
 
-
+	/** Симметричная. Вариант 12*/
+	/**	Кривошея Михаил **/
+	std::cout << "Матрица: " << std::endl << m;
+	std::cout << "Симметрична: " << (m.isSymmetric() ? "Нет" : "Да") << std::endl << std::endl;
 }
 
 void
@@ -22,23 +21,23 @@ test_is_Minor()
 	/** Минор. Вариант 13*/
 	/**	Панасенко Андрей **/
 	Matrix <int> m(4, 4, 0);
-	for (int i = 0, i2 = 0; i < m._value.size(); i++, i2++) {
-		for (int j = 0, j2 = 0; j < m._value.size(); j++, j2++) {
-			m.put(i2, j2, m._value[i][j]);
+	for (int i = 0, i2 = 0; i < m.getRowCount(); i++, i2++) {
+		for (int j = 0, j2 = 0; j < m.getColCount(); j++, j2++) {
+			m.put(i2, j2, m.get(i, j));
 		}
 	}
-	
+
 	int row = 3, col = 3;
-	std::cout << "MATRIX: " << m << std::endl;
-	std::cout << "Determinant minor: " << m.minor(row, col) << std::endl;
+	std::cout << "Матрица: " << std::endl << m;
+	std::cout << "Минор (" << row << "," << col << ") = " << m.minor(row, col) << std::endl << std::endl;
 }
 
 int
 main(int argc, char **argv)
 {
 	setlocale(0, "Russian");
-    __test_isSymmetric();
+	__test_isSymmetric();
 	test_is_Minor();
-    system("pause");
-    return EXIT_SUCCESS;
+	system("pause");
+	return EXIT_SUCCESS;
 }
