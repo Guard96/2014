a4febb6329ce0e5648f69fd2d35c891275622f96
folopanda <folomant@yandex.ru>
2015-06-07 13:48:56 +0300

    Update matrix.h

--- ivb-3-14/Хлебников.А.А/Лабораторная работа №8/Matrix/Matrix/matrix.h
+++ ivb-3-14/Хлебников.А.А/Лабораторная работа №8/Matrix/Matrix/matrix.h
@@ -156,6 +156,42 @@ public:
 	}
 
 	/**Обратная матрица. Вариант 8 (2)*/
+	/* Folomkin A.A. */
+	Matrix<_Type> invert()
+	{
+		int size = getRowCount();
+		if (size != getColCount()){
+			throw MatrixException("Invalid size");
+			return *this;
+			
+		}
+	Matrix<_Type> result(size, size, 0);
+	_Type det = determinant();
+	
+	if (det == 0)
+	{
+		throw MatrixException("Determinant = 0");
+		return *this;
+		
+	}
+	_Type temp;
+	bool _invert = false;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			temp = _invert ? 0 : 1;
+			temp = minor(i, j);
+			temp /= det;
+			result.put(i, j, temp);
+			_invert = !_invert;
+			
+		}
+		
+	}
+	result = result.transposition();
+	return result;
+		
+	}
+	
 	Matrix<_Type> & invert()
 	{
 		return *this;
