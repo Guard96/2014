c97227737b95bc249af40b86c330abec36cede87
Guard96 <shod-100@mail.ru>
2015-06-10 11:17:28 +0300

    Update Laba4.cpp

--- ivb-3-14/Шмидт М.И/Laba4.cpp
+++ ivb-3-14/Шмидт М.И/Laba4.cpp
@@ -10,21 +10,21 @@
 #include <fstream>
 using namespace std;
 
-int igra(int data[], int left, int rigth, int player, int scope1, int scope2){
-	if (left <= rigth){
-		int il = igra(data,left+1, rigth,3-player, scope2, scope1 + data[left]);
-		int ir = igra(data,left, rigth-1, 3-player, scope2, scope1 + data[rigth]);
+int igra(int data[], int left, int rigth, int player, int scope1, int scope2) {
+	if (left <= rigth) {
+		int il = igra(data, left + 1, rigth,3 - player, scope2, scope1 + data[left]);
+		int ir = igra(data, left, rigth - 1, 3 - player, scope2, scope1 + data[rigth]);
 		if (il == player || ir == player)
 			return player;
 		else if (il == 0 || ir == 0)
 			return 0;
 		else 
-			return 3-player;
+			return 3 - player;
 	} else {
 		if (scope1 > scope2)
 			return player;
 		else if (scope1 < scope2)
-			return 3-player;
+			return 3 - player;
 		else 
 			return 0; 
 
@@ -44,7 +44,7 @@ int main() {
 	
 	ofstream fout;
 	fout.open("OUTPUT.TXT");
-	fout << igra(data, 0, n-1, 1, 0, 0);
+	fout << igra(data, 0, n - 1, 1, 0, 0);
 	fout.close();
 	return EXIT_SUCCESS;
 }
